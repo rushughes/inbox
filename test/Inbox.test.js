9272ee27f1ea3ff89fa9e1ce22c24038c21feea7
Rus Hughes <russell.hughes@gmail.com>
2018-05-14 00:03:30 +0700

    section 2 lecture 47 - fix

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -13,7 +16,9 @@ beforeEach(async () => {
   // Use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
-    .send({ from: accounts[0], gas: '1000000' })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
