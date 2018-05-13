e8c27ccbe1ae60c55fb0972ab6b1f581eb9d5a06
Rus Hughes <russell.hughes@gmail.com>
2018-05-14 01:24:25 +0700

    Section 2 Lecture 54

--- deploy.js
+++ deploy.js
@@ -3,6 +3,9 @@ const Web3= require('web3');
 const { interface, bytecode } = require('./compile');
 const { mnemonic, network } = require('./config');
 
+console.log('mnemonic:', mnemonic);
+console.log('network:', network);
+
 const provider = new HDWalletProvider(
   mnemonic,
   network
@@ -15,8 +18,12 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
-    .send({ gas: '1000000', from: accounts[0] });
+    .deploy({ data: '0x' + bytecode, arguments: ['Hi there!'] })
+    .send({
+      gas: '1000000',
+      gasPrice: web3.utils.toWei('2', 'gwei'),
+      from: accounts[0]
+    });
 
     console.log('Contract deployed to', result.options.address);
 };
